e45230f4d65b8ba7c9257fba9f5ef455d4d57b8f
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-04-24 22:57:08 -0400

    added function in my form.js also change background color on dark mode and adjusted the <script> tag on index.html

---
+++ assets/js/form.js
@@ -0,0 +1,41 @@
+//collect user input elements 
+const usernameEl = document.getElementById("username");
+const titleEl = document.getElementById("title");
+const contentEl = document.getElementById("content");
+
+//collect submit button element
+const subtmitButton = document.getElementById("submit-btn");
+
+// add event listener to submit button 
+subtmitButton.addEventListener("click", function (event) {
+event.preventDefault();
+if (validateInput()) {
+    savePost();
+}
+});
+
+//validate user input 
+const validateInput = () => {
+    if (usernameEl.ariaValueMax.trim() === `` || titleEl.ariaValueMax.trim() === ``) {
+        alert('Please enter all data!');
+        return false;
+    }
+    return true;
+};
+
+//save post to localstorage
+const savePost = () => {
+    const blogInput = {
+        username: usernameEl.value.trim(),
+        title: titleEl.value.trim(),
+        content: contentEl.value.trim()
+    };
+
+    let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+    blogPosts.push(blogInput);
+
+    localStorage.setItem('blogPosts', Json.stringify(blogPosts));
+    console.log('post saved to local storage!');
+
+    window.location.href = 'blog.html';
+};
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,12 +7,12 @@ themechange.addEventListener('click', function () {
     if (mode === 'light') {
         mode = 'dark';
         lightmode.className = 'dark';
-        themechange.textContent = <i class="fa-solid fa-moon"></i>
+        themechange.innerHTML = '<i class="fa-solid fa-moon"></i>'
         console.log('dark mode')
     } else {
         mode = 'light';
         lightmode.className = 'light';
-        themechange.textContent = <i class="fa-solid fa-sun"></i>
+        themechange.innerHTML = '<i class="fa-solid fa-sun"></i>'
         console.log('light mode');
     }
 });
