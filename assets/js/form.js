fc367544dd0f4f1d88725e93ff07370ac0ef9bc0
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-04-28 00:48:55 -0400

    app is functioning properly will test again in the morning and finish README

--- assets/js/form.js
+++ assets/js/form.js
@@ -62,3 +62,68 @@ document.addEventListener('DOMContentLoaded', function () {
     };
     attachSubmitListener();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    //collect user input elements 
+    const usernameEl = document.getElementById("username");
+    const titleEl = document.getElementById("title");
+    const contentEl = document.getElementById("content");
+
+    // add event listener to submit button 
+    const attachSubmitListener = () => {
+        const submitButton = document.getElementById("submit-btn");
+        if (submitButton) {
+            submitButton.addEventListener("click", submitHandler);
+        } else {
+            console.error('submit button not found')
+        }
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if (validateInput()) {
+            savePost();
+            clearInputFields();
+            alert('Post Saved Successfully!');
+            return false
+        }
+    };
+    // // call function to attach event listener 
+    // attachSubmitListener();
+
+    //function to clear input fields
+    const clearInputFields = () => {
+        usernameEl.value = '';
+        titleEl.value = '';
+        contentEl.value = '';
+    };
+
+
+    //validate user input 
+    const validateInput = () => {
+        console.log('Validatin Input...')
+        if (usernameEl.value.trim() === `` || titleEl.value.trim() === ``) {
+            alert('Please enter all data!');
+            return false;
+        }
+        return true;
+    };
+
+    //save post to localstorage
+    const savePost = () => {
+        const blogInput = {
+            username: usernameEl.value.trim(),
+            title: titleEl.value.trim(),
+            content: contentEl.value.trim()
+        };
+
+        let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+        blogPosts.push(blogInput);
+
+        localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
+        console.log('post saved to local storage!');
+
+        window.location.href = 'blog.html';
+    };
+    attachSubmitListener();
+});
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,3 +16,29 @@ themechange.addEventListener('click', function () {
         console.log('light mode');
     }
 });
+
+// //function to display blog posts from local storage
+// function displayPosts() {
+//     const blogPosts = JSON.parse(localStorage.getItem('Blog-Posts')) || [];
+//     console.log(blogPosts);
+//         const blogPostsContainer = document.getElementById('blog-posts');
+//     blogPostsContainer.innerHTML = '';
+
+//     blogPosts.forEach(post => {
+//         const postElement = document.createElement('div');
+//         postElement.innerHTML = `
+//         <h3>${post.title}</h3>
+//         <p><strong>Author:</strong> ${post.username}</p>
+//         <p>${post.content}</p>
+//         `;
+//         blogPostsContainer.appendChild(postElement);
+//     });
+// }
+
+// //Event listener for the display post btn
+// const displayPostsBtn = document.getElementById('display-post-btn');
+// if (displayPostsBtn) {
+// displayPostsBtn.addEventListener('click', displayPosts);
+// } else {
+//     console.error('Element with Id Display-post-btn not found!!')
+// }
